3aab8f04d466bfc2bc89e8bf66feca7038cb581b
Chris <apstld@gmail.com>
2025-10-27 00:43:26 +0200

    Force full push 27-Mon-10_0-43

--- workers/worker_pph.py
+++ workers/worker_pph.py
@@ -6,8 +6,7 @@ import asyncio
 import logging
 from datetime import datetime, timezone
 from platform_peopleperhour import fetch_pph_jobs
-from utils import send_job_to_user
-from utils_fx import time_ago
+from utils import send_job_to_user, time_ago
 from currency_usd import convert_to_usd
 from db import get_user_keywords
 
