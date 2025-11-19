856e25e8cb408aaad18b368f7360a2a821578392
Chris <apstld@gmail.com>
2025-11-19 13:40:07 +0200

    Auto push by double-click

--- worker.py
+++ worker.py
@@ -51,17 +51,6 @@ def fetch_all(keywords_query: Optional[str] = None) -> List[Dict]:
 
 
 
-    # --- PEOPLEPERHOUR ---
-    import platform_peopleperhour as pph
-    if PLATFORMS.get("peopleperhour"):
-        try:
-            kws = _normalize_kw_list(keywords_query.split(",") if keywords_query else [])
-            for i in pph.get_items(kws):
-                i["affiliate"] = False
-                out.append(i)
-        except Exception:
-            pass
-
 
     if PLATFORMS.get("skywalker"):
         try:
